027fcddaf9daf2d5820dd577055a892692817eb7
WumaCoder <15804854160@163.com>
2022-02-06 20:39:12 +0800

    chore(nest): init env

---
+++ packages/nest/rollup.config.js
@@ -0,0 +1,20 @@
+import ts from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "lib/coussin-nest.cjs.js",
+      format: "cjs",
+    },
+    {
+      file: "lib/coussin-nest.es.js",
+      format: "es",
+    },
+  ],
+  plugins: [
+    nodeResolve({ extensions: [".ts", ".js"] }),
+    ts({ outputToFilesystem: false }),
+  ],
+};
